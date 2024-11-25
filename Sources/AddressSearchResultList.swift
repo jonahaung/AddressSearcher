b9b5a588aa5a6295332fbee5375b9428fbb0f338
Aung Ko Min <108913030+jonahaung2@users.noreply.github.com>
2024-11-26 00:20:49 +0800

    enhancements

--- Sources/AddressSearchResultList.swift
+++ Sources/AddressSearchResultList.swift
@@ -6,27 +6,67 @@
 //
 
 import SwiftUI
+import MapKit
 
 public struct AddressSearchResultList: View {
     
     @State private var viewModel: AddressSearchViewModel
+    private let onSelect: (MKMapItem) -> Void
     
-    public init(searchText: DebouncedText, _ configuration: AddressSearchConfiguration = .shared) {
+    public init(
+        searchText: DebouncedText,
+        _ configuration: AddressSearchConfiguration = .shared,
+        _ onSelect: @escaping (MKMapItem) -> Void
+    ) {
         viewModel = .init(searchText: searchText, configuration)
+        self.onSelect = onSelect
     }
     
     public var body: some View {
         List {
-            ForEach(viewModel.searchSuggestions) { suggestion in
-                Text(suggestion.highlightedTitleStringForDisplay)
-                    .onTapGesture {
-                        viewModel.updateSearchResults(for: suggestion)
+            if !viewModel.searchSuggestions.isEmpty {
+                Section {
+                    ForEach(viewModel.searchSuggestions) { suggestion in
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 18, height: 18)
+                            Button {
+                                viewModel.updateSearchResults(for: suggestion)
+                            } label: {
+                                Text(suggestion.highlightedTitleStringForDisplay)
+                            }
+                        }
                     }
+                }
             }
-            ForEach(viewModel.results, id: \.self) { result in
-                Text(result.placemark.formattedAddress ?? result.description)
+            if !viewModel.results.isEmpty {
+                Section {
+                    ForEach(viewModel.results, id: \.self) { result in
+                        HStack {
+                            Image(systemName: "mappin.and.ellipse")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 18, height: 18)
+                            Button {
+                                onSelect(result)
+                            } label: {
+                                Text(result.placemark.formattedAddress ?? result.description)
+                            }
+                        }
+                    }
+                } header: {
+                    Text("Results")
+                }
+            }
+            if viewModel.debouncedText.text.isEmpty == false && viewModel.results.isEmpty && viewModel.searchSuggestions.isEmpty {
+                Section {
+                    ContentUnavailableView.search
+                }
             }
         }
+        .buttonStyle(.plain)
         .onAppear {
             viewModel.startGeneratingSearchCompletions()
         }
